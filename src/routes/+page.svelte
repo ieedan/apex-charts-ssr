d9519b9762809d61496c0bd6b9aa65b053c97ee2
Aidan Bleser <aidanbleser35@gmail.com>
2024-02-29 15:44:28 -0600

    Intial Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    // If you import ApexCharts server returns 500 because window doesn't exist
+    import ApexCharts from "apexcharts";
+	import { onMount } from "svelte";
+
+	onMount(() => {
+		var options = {
+			chart: {
+				type: "line",
+			},
+			series: [
+				{
+					name: "sales",
+					data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+				},
+			],
+			xaxis: {
+				categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+			},
+		};
+
+		var chart = new ApexCharts(document.querySelector("#chart"), options);
+
+		chart.render();
+	});
+</script>
+
+<div id="chart"></div>
